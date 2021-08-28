35497e8a437478f6cd8f4b649ac3b1be785c4061
G'ulomjon Bakirov <80908091+PandaCoderSudo@users.noreply.github.com>
2021-08-28 14:17:32 -0400

    Search section

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,26 +1,60 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 
+const regions = [
+  {
+    name: "Tashkent",
+  },
+  {
+    name: "Jizzakh",
+  },
+  {
+    name: "Andijan",
+  },
+  {
+    name: "Samarkand",
+  },
+  {
+    name: "Surkhandaryo",
+  },
+
+  {
+    name: "Namangan",
+  },
+  {
+    name: "Nukus",
+  },
+  {
+    name: "Khiva",
+  },
+  {
+    name: "Navoiy",
+  },
+
+  {
+    name: "Bukhara",
+  },
+  {
+    name: "Denov",
+  },
+  {
+    name: "Qarshi",
+  },
+];
+
 export default function Rightbar() {
   return (
     <div className="rightbarContainer">
       <div className="rightbarSearch">
-        <input className="search" />
+        <input className="search" placeholder="Search Your Location" />
         <SearchIcon className="searchIcon" />
       </div>
       <ul className="rightbarList">
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
-        <li className="rightbarItem">Tashkent</li>
+        {regions.map((region, index) => (
+          <li key={index} className="rightbarItem">
+            {region.name}
+          </li>
+        ))}
       </ul>
       <hr />
       <h4>Weather Details</h4>
